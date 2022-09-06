efdab61e6eb7f56c38c1dbab4ae98220106a0343
Project X <luzhnov_aleksei@mail.ru>
2022-09-06 18:52:57 +0300

    3 залив, спрятал ключ Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&t5a@4#x0r-kf5-9qul=hi7opd$@n+88(c2!55w^40hx8iy50'
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
